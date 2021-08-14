22c52149ba0ac3c9373b948415fba30f84daf43d
Phudevsxz <86899476+Phudevsxz@users.noreply.github.com>
2021-08-14 03:29:25 -0700

    >.<

---
+++ app/src/main/java/com/example/mychatapp2/Model/Chatslist.java
@@ -0,0 +1,20 @@
+package com.example.mychatapp2.Model;
+
+public class Chatslist {
+    String id;
+
+    public Chatslist(String id) {
+        this.id = id;
+    }
+
+    public Chatslist() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
